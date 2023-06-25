7c606cdc79e84659507b3f685a0913f7a6ebe0a9
ajithmohananunni <137688731+ajithmohananunni@users.noreply.github.com>
2023-06-25 22:27:26 +0000

    Update AMUSEBOX'.py

--- AMUSEBOX'.py
+++ AMUSEBOX'.py
@@ -10,8 +10,8 @@ import datetime
 from base64 import b64decode,b64encode
 from datetime import date
 
-expirydate = datetime.date(2023, 21, 12)
-#expirydate = datetime.date(2023,21, 12)
+expirydate = datetime.date(2024,12, 21)
+#expirydate = datetime.date(2024,12, 20)
 today=date.today()
 def hero():
 
